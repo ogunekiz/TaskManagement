225f552f082b05f099ef3094c480013c86f70e65
Ogün EKİZ <ogunekiz@hotmail.com>
2024-11-26 17:38:22 +0300

    1. The project's Task class was created in TaskManagement.Entity. 2. The project's context class was created in TaskManagement.Dal. 3. Context was added to the Program.cs file. 4. Connectionstrings information was added to the appsettings.json file. 5. The migration file was created and reflected in the database.

---
+++ TaskManagement.Dal/DataContext/TaskManagementDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Task = TaskManagement.Entity.Concrete.Task;
+
+namespace TaskManagement.Dal.DataContext
+{
+    public class TaskManagementDbContext : DbContext
+    {
+        public TaskManagementDbContext(DbContextOptions<TaskManagementDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
